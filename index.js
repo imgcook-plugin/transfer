21e34e12472be0ebadd56673c4fc9487e41d7a76
徐镜渊 <jingyuan.xu@mihoyo.com>
2020-02-19 18:41:56 +0800

    test

--- index.js
+++ index.js
@@ -8,24 +8,24 @@
 
 const pluginHandler = async options => {
   let { data, filePath, config  } = options;
-  if(Array.isArray(data.code.panelDisplay)) {
-    data.code.panelDisplay.forEach(element => {
-      // const reg = /(\d+\.([0-9]{0,2})px)|(\d+px)/g;
-      const reg = /(^\d+\.([0-9]{0,2})px$)|(^\d+px$)/g;
-      // if(element.panelName === 'style.js') {
-      if(element.panelName === 'index.css') {
-        element.panelValue = element.panelValue.replace(reg, function (match,param,offset,string) {
-          if(!isNaN(Number(match.replace('px', '')))) {
-            return `${Number(match.replace('px', '')) / 100}rem`
-          }
-          return match;
-        });
-      }
-      // if(element.panelName === 'index.vue') {
-      //   element.panelValue = element.panelValue.replace('index.response.css', 'index.css');
-      // }
-    });
-  }
+  // if(Array.isArray(data.code.panelDisplay)) {
+  //   data.code.panelDisplay.forEach(element => {
+  //     // const reg = /(\d+\.([0-9]{0,2})px)|(\d+px)/g;
+  //     const reg = /(^\d+\.([0-9]{0,2})px$)|(^\d+px$)/g;
+  //     // if(element.panelName === 'style.js') {
+  //     if(element.panelName === 'index.css') {
+  //       element.panelValue = element.panelValue.replace(reg, function (match,param,offset,string) {
+  //         if(!isNaN(Number(match.replace('px', '')))) {
+  //           return `${Number(match.replace('px', '')) / 100}rem`
+  //         }
+  //         return match;
+  //       });
+  //     }
+  //     // if(element.panelName === 'index.vue') {
+  //     //   element.panelValue = element.panelValue.replace('index.response.css', 'index.css');
+  //     // }
+  //   });
+  // }
   return { data, filePath, config };
 };
 
