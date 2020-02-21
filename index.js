acaa82e0cbe6bbbd0d93fa43e1e5c18664ab7b1f
徐镜渊 <jingyuan.xu@mihoyo.com>
2020-02-21 11:25:50 +0800

    开放vue更改

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 const pluginHandler = async options => {
   let { data, filePath, config  } = options;
   if(Array.isArray(data.code.panelDisplay)) {
-    data.code.panelDisplay = data.code.panelDisplay.map(element => {
+    data.code.panelDisplay = [...data.code.panelDisplay.map(element => {
       // const reg = /(\d+\.([0-9]{0,2})px)|(\d+px)/g;
       const reg = /(^\d+\.([0-9]{0,2})px$)|(^\d+px$)/g;
       // if(element.panelName === 'style.js') {
@@ -25,8 +25,9 @@ const pluginHandler = async options => {
         element.panelValue = element.panelValue.replace('index.response.css', 'index.css');
       }
       return element;
-    });
+    })];
   }
+  console.log('嘻嘻嘻', data.code.panelDisplay);
   return { data, filePath, config };
 };
 
