39b0c4ab5e6cd24ea25b8065fd9f3f6f19898aba
徐镜渊 <jingyuan.xu@mihoyo.com>
2020-03-17 10:29:26 +0800

    将index.css更改为px单位

--- index.js
+++ index.js
@@ -12,42 +12,61 @@ const loadToLocal = require('./lib/download').loadToLocal;
 const filePaths = './src/resources/images';
 
 const pluginHandler = async options => {
-  let { data, filePath, config  } = options;
-  if(Array.isArray(data.code.panelDisplay)) {
-    data.code.panelDisplay = [...data.code.panelDisplay.map(element => {
-      // const reg = /(\d+\.([0-9]{0,2})px)|(\d+px)/g;
-      const imgReg = /(https?):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|](\.png|\.jpg)/g;
-      const reqReg = /\'require\(\'[&@#/%?=~_A-Za-z0-9]*(\.png|\.jpg)\'\)\'/g;
-      if(element.panelName === 'index.vue') {
-        element.panelValue = 
-          element.panelValue.replace(imgReg, function (match,param,offset,string) {
-            // 如果不存在根文件夹，则直接返回url
-            
-            const needPath = path.resolve().indexOf('src') > -1 ?  path.resolve().split('src')[0] : path.resolve();
-            console.log('嘻嘻嘻', path.resolve(needPath, filePaths));
-            if(!fs.existsSync(path.resolve(needPath, filePaths))) {
-              return match;
+  let { data, filePath, config } = options;
+  if (Array.isArray(data.code.panelDisplay)) {
+    data.code.panelDisplay = [
+      ...data.code.panelDisplay.map(element => {
+        const reg = /(\d+\.([0-9]{0,2})rem)|(\d+rem)/g;
+        const imgReg = /(https?):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|](\.png|\.jpg)/g;
+        const reqReg = /\'require\(\'[&@#/%?=~_A-Za-z0-9]*(\.png|\.jpg)\'\)\'/g;
+        if (element.panelName === 'index.vue') {
+          element.panelValue = element.panelValue
+            .replace(imgReg, function(match, param, offset, string) {
+              // 如果不存在根文件夹，则直接返回url
+
+              const needPath =
+                path.resolve().indexOf('src') > -1
+                  ? path.resolve().split('src')[0]
+                  : path.resolve();
+              console.log('当前目录：', path.resolve(needPath, filePaths));
+              if (!fs.existsSync(path.resolve(needPath, filePaths))) {
+                return match;
+              }
+              loadToLocal(
+                match,
+                `${path.resolve(needPath, filePaths)}/${data.moduleData.id}`,
+                `${data.moduleData.id}`,
+                `${data.moduleData.id}_${string}${path.extname(match)}`
+              );
+              return `require('@images/${
+                data.moduleData.id
+              }/${data.moduleData.id}_${string}${path.extname(match)}')`;
+            })
+            .replace(reqReg, function(match) {
+              return match.slice(1, match.length - 1);
+            });
+          // if(element.panelName === 'index.vue') {
+          //   element.panelValue = element.panelValue.replace('index.response.css', 'index.css');
+          // }
+        }
+        if (element.panelName === 'index.css') {
+          element.panelValue = element.panelValue.replace(reg, function(
+            match,
+            param,
+            offset,
+            string
+          ) {
+            if (!isNaN(Number(match.replace('rem', '')))) {
+              return `${Number(match.replace('rem', '')) * 100}px`;
             }
-            loadToLocal(match, `${path.resolve(needPath, filePaths)}/${data.moduleData.id}`, `${data.moduleData.id}` ,`${data.moduleData.id}_${string}${path.extname(match)}`);
-            return `require('@images/${data.moduleData.id}/${data.moduleData.id}_${string}${path.extname(match)}')`;
-          }).replace(reqReg, function(match) {
-            return match.slice(1,match.length-1);
-          })
-      // if(element.panelName === 'index.css') {
-      //   element.panelValue = element.panelValue.replace(reg, function (match,param,offset,string) {
-      //     if(!isNaN(Number(match.replace('px', '')))) {
-      //       return `${Number(match.replace('px', '')) / 100}rem`
-      //     }
-      //     return match;
-      //   });
-      // }
-      // if(element.panelName === 'index.vue') {
-      //   element.panelValue = element.panelValue.replace('index.response.css', 'index.css');
-      // }
-      return element;
-    }})];
+            return match;
+          });
+        }
+        return element;
+      })
+    ];
   }
-  console.log(data.code);
+  // console.log(data.code);
   return { data, filePath, config };
 };
 
